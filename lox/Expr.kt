390e5cb334d5cd6cf0529d928a4bb2a29bddf8ff
BKinya <Gitongakinya1@gmail.com>
2026-05-13 17:36:19 +0200

    fix data class

--- lox/Expr.kt
+++ lox/Expr.kt
@@ -2,7 +2,7 @@ package lox
 
 sealed interface Expr
 
-class Ternary(val condition: Expr, val thenBranch: Expr, val elseBranch: Expr) : Expr
+data class Ternary(val condition: Expr, val thenBranch: Expr, val elseBranch: Expr) : Expr
 data class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr
 data class Grouping(val expression: Expr) : Expr
 data class Unary(val operator: Token, val right: Expr) : Expr
